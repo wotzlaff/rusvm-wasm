6bd94c57f706c9afd3db3dfc8a8c5679c4aa5f7a
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2024-01-16 14:56:50 +0100

    add newton

--- src/lib.rs
+++ src/lib.rs
@@ -63,6 +63,23 @@ fn extract_params_smo(params: &Object) -> (rusvm::smo::Params, usize) {
     (p, cache_size)
 }
 
+fn extract_params_newton(params: &Object) -> (rusvm::newton::Params, usize) {
+    let mut p = rusvm::newton::Params::new();
+    let cache_size: usize = if params.is_object() {
+        p.tol = get_nonan(params, "tol", p.tol);
+        p.max_steps = get_usize(params, "max_steps", p.max_steps);
+        p.verbose = get_usize(params, "verbose", p.verbose);
+        p.time_limit = get_nonan(params, "time_limit", p.time_limit);
+        p.sigma = get_nonan(params, "sigma", p.sigma);
+        p.eta = get_nonan(params, "eta", p.eta);
+        p.max_back_steps = get_usize(params, "max_back_steps", p.max_back_steps);
+        get_usize(params, "cache_size", 0)
+    } else {
+        0
+    };
+    (p, cache_size)
+}
+
 fn extract_params_problem(params: &Object) -> rusvm::problem::Params {
     let mut p = rusvm::problem::Params::new();
     if params.is_object() {
@@ -117,20 +134,7 @@ fn prepare_problem<'a>(
     }
 }
 
-#[wasm_bindgen]
-pub fn smo(
-    x: &Array,
-    y: &[f64],
-    params_problem: &Object,
-    params_smo: &Object,
-) -> Result<JsValue, serde_wasm_bindgen::Error> {
-    panic::set_hook(Box::new(console_error_panic_hook::hook));
-
-    // get parameters
-    let (params_smo, cache_size) = extract_params_smo(params_smo);
-    // prepare problem
-    let problem = prepare_problem(&y, params_problem).unwrap();
-    // prepare kernel
+fn extract_data(x: &Array) -> Vec<Vec<f64>> {
     let mut data = Vec::new();
     let mut nft = u32::MAX;
     for i in 0..x.length() {
@@ -146,12 +150,63 @@ pub fn smo(
         }
         data.push(arr);
     }
-    let base = Box::new(rusvm::kernel::gaussian_from_vecs(
+    data
+}
+
+#[wasm_bindgen]
+pub fn smo(
+    x: &Array,
+    y: &[f64],
+    params_problem: &Object,
+    params_smo: &Object,
+) -> Result<JsValue, serde_wasm_bindgen::Error> {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+    // get parameters
+    let (params_smo, cache_size) = extract_params_smo(params_smo);
+    // prepare problem
+    let problem = prepare_problem(&y, params_problem).unwrap();
+    // prepare kernel
+    let data = extract_data(x);
+    let gamma = get_nonan(params_problem, "gamma", 1.0);
+
+    let mut base = Box::new(rusvm::kernel::gaussian_from_vecs(
         data.iter().map(|v| v.as_slice()).collect::<Vec<&[f64]>>(),
-        get_nonan(params_problem, "gamma", 1.0),
+        gamma,
     ));
     let mut kernel = cache(base, cache_size);
     let result = rusvm::smo::solve(problem.as_ref(), kernel.as_mut(), &params_smo, None);
+    let (result_sv, svs) = result.find_support(&data);
+    serde_wasm_bindgen::to_value(&(result.opt_status, result_sv, svs))
+}
+
+#[wasm_bindgen]
+pub fn newton(
+    x: &Array,
+    y: &[f64],
+    params_problem: &Object,
+    params_newton: &Object,
+) -> Result<JsValue, serde_wasm_bindgen::Error> {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+    // get parameters
+    let (params_newton, cache_size) = extract_params_newton(params_newton);
+    // prepare problem
+    let problem = prepare_problem(&y, params_problem).unwrap();
+    // prepare kernel
+    let data = extract_data(x);
+    let gamma = get_nonan(params_problem, "gamma", 1.0);
+
+    let mut base = Box::new(rusvm::kernel::gaussian_from_vecs(
+        data.iter().map(|v| v.as_slice()).collect::<Vec<&[f64]>>(),
+        gamma,
+    ));
+    let mut kernel: Box<dyn Kernel> = if cache_size > 0 {
+        Box::new(rusvm::kernel::CachedKernel::from(base.as_mut(), cache_size))
+    } else {
+        base
+    };
+    let result = rusvm::newton::solve(problem.as_ref(), kernel.as_mut(), &params_newton, None);
     let (result_sv, svs) = result.find_support(&data);
     serde_wasm_bindgen::to_value(&(result.opt_status, result_sv, svs))
 }
